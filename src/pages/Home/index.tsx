937d7cb30847179c03183071ec547254d4eb2c0d
Isaac Chaneel Lee <gssisaac@gmail.com>
2022-12-07 16:09:16 +0900

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,16 @@ export function Home() {
   return (
     <div className='flex flex-col'>
       <span className='typo-title'>Welcome, home</span>
+      <div className='flex flex-row gap-5'>
+        <div className='w-32 p-4 m-4 text-gray-900 bg-gray-300 border border-gray-600 rounded-lg'>
+          hello
+        </div>
+
+        <div>
+          hello2
+        </div>
+      </div>
+
     </div>
   )
 }
